351211b03977e13998bb5ab69883b3acffeb2201
Victoria Shanukova <vshanukova@gmail.com>
2022-04-12 21:23:05 +0100

    Add NetworkError comp

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -57,8 +57,8 @@ export default {
           this.events = response.data
           this.totalEvents = response.headers['x-total-count']
         })
-        .catch(error => {
-          console.log(error)
+        .catch(() => {
+          this.$router.push({ name: 'NetworkError' })
         })
     })
   },

ac694036c7f82a0bccf1c57d613ed94b0278d7c1
Victoria Shanukova <vshanukova@gmail.com>
2022-04-18 16:04:26 +0100

    Restart local repo due to package issue

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp, reactive } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+const GStore = reactive({ flashMessage: '' })
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .provide('GStore', GStore)
+  .mount('#app')
---
+++ src/views/event/Edit.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>Edit the event here</p>
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  data: function() {
+    return {
+      unsavedChanges: false  // <-- Flag gets set to true if anything
+                             //          is changed on the form
+    }
+  },
+  beforeRouteLeave(routeTo, routeFrom) {
+    if (this.unsavedChanges) {
+      const answer = window.confirm(
+        'Do you really want to leave? You have unsaved changes!'
+      )
+      if (!answer) {
+        return false // <-- Cancels the navigation
+      }
+    }
+  }
+}
+</script>

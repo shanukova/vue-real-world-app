5fd1f4f9023e68a825506a612c87e10f572ed9ed
Victoria Shanukova <vshanukova@gmail.com>
2022-04-20 13:57:34 +0100

    Return back EventService.vue

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -10,30 +10,10 @@ const apiClient = axios.create({
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(perPage, page) {
+    return apiClient.get(`/events?_limit=${perPage}&_page=${page}`)
   },
   getEvent(id) {
-    return apiClient.get('/events/' + id)
+    return apiClient.get(`/events/${id}`)
   }
 }
-
-// import axios from 'axios'
-
-// const apiClient = axios.create({
-//   baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
-//   withCredentials: false,
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json'
-//   }
-// })
-
-// export default {
-//   getEvents(perPage, page) {
-//     return apiClient.get(`/events?_limit=${perPage}&_page=${page}`)
-//   },
-//   getEvent(id) {
-//     return apiClient.get(`/events/${id}`)
-//   }
-// }
